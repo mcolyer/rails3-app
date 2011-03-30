2e5d9a43e563a25d3800a6b3540a0bfb9b401e5f
Matt Colyer <matt@colyer.name>
2011-03-29 22:46:02 -0700

    Update choices.

--- app.rb
+++ app.rb
@@ -1,23 +1,26 @@
 rvmrc = <<-RVMRC
 rvm_gemset_create_on_use_flag=1
-rvm gemset use #{app_name}
+rvm use 1.9.2@#{app_name}
 RVMRC
 
 create_file ".rvmrc", rvmrc
 
-gem "rspec-rails", ">= 2.0.0.beta.22", :group => :test
-gem "factory_girl_rails", ">= 1.0.0", :group => :test
-gem "capybara", ">= 0.3.9", :group => :test
-gem "steak", ">= 0.4.0.a5", :group => :test
-gem "factory_girl_generator", ">= 0.0.1", :group => [:test, :development]
-gem "haml-rails", ">= 0.2"
-gem "compass", ">= 0.10.5"
-gem "compass-susy-plugin", ">= 0.8.1"
-gem "devise", ">= 1.1.3"
+gem "barista"
+gem "compass"
+gem "compass-susy-plugin"
+gem "haml-rails"
+gem "jquery-rails"
+gem "rspec-rails", :group => :test
+gem "factory_girl_rails", :group => :test
+gem "capybara", :group => :test
+gem "factory_girl_generator", :group => [:test, :development]
+gem "fixture_builder", :group => [:test, :development]
+gem "rails3-generators", :group => :development
 
 generators = <<-GENERATORS
 
     config.generators do |g|
+      g.fixture_replacement :factory_girl
       g.test_framework :rspec, :fixture => true, :views => false
       g.integration_tool :rspec, :fixture => true, :views => true
     end
@@ -25,26 +28,7 @@ GENERATORS
 
 application generators
 
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
-
 gsub_file 'config/application.rb', 'config.action_view.javascript_expansions[:defaults] = %w()', 'config.action_view.javascript_expansions[:defaults] = %w(jquery.js jquery-ui.js rails.js)'
-
-layout = <<-LAYOUT
-!!!
-%html
-  %head
-    %title #{app_name.humanize}
-    = stylesheet_link_tag :all
-    = javascript_include_tag :defaults
-    = csrf_meta_tag
-  %body
-    = yield
-LAYOUT
-
-remove_file "app/views/layouts/application.html.erb"
-create_file "app/views/layouts/application.html.haml", layout
 
 create_file "log/.gitkeep"
 create_file "tmp/.gitkeep"
@@ -59,8 +43,7 @@ Run the following commands to complete the setup of #{app_name.humanize}:
 % cd #{app_name}
 % bundle install
 % rails generate rspec:install
-% rails generate devise:install
-% rails generate devise MODEL
+% rails generate jquery:install
 DOCS
 
 log docs
